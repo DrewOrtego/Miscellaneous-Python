5e4d4c10b91e97b86d122ca1e79d571bf1987acd
DrewOrtegoGIS <DrewOrtegoGIS@gmail.com>
2016-06-13 15:07:50 -0700

    Add files via upload

---
+++ Text-Based-Game-Engine/formatOutput.py
@@ -0,0 +1,37 @@
+''' formatOutput.py
+    
+    Prints output the console formatted to fit the Game object's consoleWidth
+    property.
+
+    TODO: currently hard coding the console width until the final platform
+    for the game is established. (Testing is being done in Windows cmd.)
+'''
+
+import os, textwrap, math
+import setup
+
+def printHeader():
+    ''' Prints the header showing the current room and number of moves.'''
+    print("{0:<59} Moves: {1}".format(
+        setup.objects['CurrentRoom'].name,
+        setup.objects['Game'].completed_moves)
+    )
+    print('-' * 70, '\n')
+
+
+def formatOutput(*message, newline=False):
+    ''' Formats the output of an on-screen message to fit the screen width.
+        Also calculates the number of rows to write on-screen and ensure that
+        the prompt is always in the same place.'''
+    #input() # DBUG allows tester to see any messages being overwritten
+    os.system('cls')
+    printHeader()
+    message_length = 0
+    for m in message:
+        message_length += len(m)
+        split_msg = " ".join(m.split())
+    rows = math.ceil(message_length/70)
+    print(textwrap.fill(split_msg, 70)) # Hard code width for now
+    for r in range(1, 7-rows):
+        print('')
+
---
+++ Text-Based-Game-Engine/game.py
@@ -0,0 +1,54 @@
+''' Game.py
+'''
+
+from formatOutput import formatOutput as print
+
+class Game:
+    def __init__(self):
+        self.brief_msg = False
+        self.completed_moves = 0
+        self.console_width = 70
+        self.current_room = None
+        self.prompt_char = "\n> "
+        self.tutorial_mode = None
+        self.verbose_msg = True
+        
+    @property
+    def current_room(self):
+        return self._current_room
+        
+
+    @current_room.setter
+    def current_room(self, new_room):
+        self._current_room = new_room
+
+            
+    def print_header(self):
+        ''' Prints the current room's name and number of moves.'''
+        move_msg = "moves: " + str(self.moves)
+        spaces = self.console_width -len(self.current_room.name) -len(move_msg)
+        print(); print()
+        print(self.current_room.name + (" " * spaces) + move_msg)
+        print("-" * self.console_width)
+    
+        
+    def tutorial_choice(self):
+        print('''Welcome to the game demo! If you haven't played this
+            before then there are a few things you'll need to know. If you have
+            played before then you can totally skip this intro stuff.''')
+        while self.tutorial_mode == None:
+            prompt = input('''Have you played this game before? (y/n) ''' + \
+                self.prompt_char)
+            if prompt.lower() in ['yes', 'y']:
+                self.tutorial_mode = False
+            elif prompt.lower() in ['no', 'n']:
+                self.tutorial_mode = True
+            else:
+                print('''I didn't understand your input. Type 'yes' or 
+                    'no' and press Enter.''')
+        
+    def win(self):
+        print("Congrats! You've completed the game!")
+        raise SystemExit
+
+        
